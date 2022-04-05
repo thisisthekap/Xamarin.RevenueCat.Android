05c05f4d3ff3766ebc2882f1a50805c16830028f
Christian Kapplmüller <christian.kapplmueller@tonestro.com>
2022-04-05 15:17:56 +0200

    fixed android ci

--- build-nugets.sh
+++ build-nugets.sh
@@ -1,26 +1,13 @@
 #!/bin/bash
-projects=" \
-	Xamarin.RevenueCat.Android.Core.Strings \
-	Xamarin.RevenueCat.Android.Core.Utils \
-	Xamarin.RevenueCat.Android.Core.Public \
-	Xamarin.RevenueCat.Android.Core.Common \
-	Xamarin.RevenueCat.Android.Core.Feature.Subscriber.Attributes \
-	Xamarin.RevenueCat.Android.Core.Feature.Identity \
-	Xamarin.RevenueCat.Android.Store.Google \
-	Xamarin.RevenueCat.Android \
-	Xamarin.RevenueCat.Android.Store.Amazon \
-	Xamarin.RevenueCat.Android.Extensions"
+projects=$(cat ./projects.txt)
 
-mkdir -p nugetoutput
-rm nugetoutput/*.nupkg
-
-for project in $projects; do
-	cd $project
+for project in ${projects}; do
+	cd ${project}
 	rm nugetoutput/*.nupkg
 	msbuild /p:Configuration=Release /t:Clean
 	msbuild /p:Configuration=Release /t:Restore
 	msbuild /p:Configuration=Release /t:Build
 	msbuild /p:Configuration=Release /t:Pack
-	cp nugetoutput/*.nupkg ../nugetoutput/
 	cd ..
 done
+./copy-nugets.sh
